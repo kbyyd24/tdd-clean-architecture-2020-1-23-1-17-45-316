19429457b1bb7f88425b8327591385906ab97f16
Yuexiang Gao <yxgao@thoughtworks.com>
2020-01-31 21:17:53 +0800

    implement use case to query user with pagination

--- jersey-with-mybatis/src/main/java/study/huhao/demo/application/QueryUserUseCase.java
+++ jersey-with-mybatis/src/main/java/study/huhao/demo/application/QueryUserUseCase.java
@@ -3,8 +3,10 @@ package study.huhao.demo.application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import study.huhao.demo.domain.contexts.usercontext.user.User;
+import study.huhao.demo.domain.contexts.usercontext.user.UserCriteria;
 import study.huhao.demo.domain.contexts.usercontext.user.UserRepository;
 import study.huhao.demo.domain.contexts.usercontext.user.UserService;
+import study.huhao.demo.domain.core.common.Page;
 
 @Component
 public class QueryUserUseCase {
@@ -18,5 +20,10 @@ public class QueryUserUseCase {
 
   public User get(String id) {
     return userService.get(id);
+  }
+
+  public Page<User> query(int limit, int offset) {
+    UserCriteria criteria = new UserCriteria(limit, offset);
+    return userService.query(criteria);
   }
 }
